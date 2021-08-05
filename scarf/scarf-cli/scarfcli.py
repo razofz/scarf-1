27236ff7a688adfd5437ca09280b8c43e764175d
razofz <rasmus.olofzon@gmail.com>
2021-08-05 13:34:59 +0200

    basic scRNA-seq pipeline works now
    Basically, the PoC works. I. e., the functionality from the first
    vignette is there. Next possible steps are making the interface nicer,
    exposing the different steps spearately (to allow for e. g. choosing
    filter values yourself) and making sure that the other example datasets
    works as well.

--- scarf/scarf-cli/scarfcli.py
+++ scarf/scarf-cli/scarfcli.py
@@ -1,6 +1,7 @@
 import click
 import scarf
 import pandas as pd
+import os
 
 # functionality to support:
 # - fetch_dataset (INPUT: what dataset to fetch)
@@ -51,6 +52,7 @@ class Scarf_cli(object):
     def __init__(self, dataset, output_dir, nthreads):
         self.dataset = dataset
         self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
         self.datastore = None
         if nthreads is not None:
             self.nthreads=nthreads
@@ -60,12 +62,14 @@ class Scarf_cli(object):
     def __str__(self):
         return f'Dataset: {self.dataset}, Output dir: {self.output_dir}'
     
-    def download_dataset():
-        scarf.fetch_dataset(self.dataset, save_path=self.outputdir)
+    def download_dataset(self):
+        click.echo(f'> Downloading dataset ' + click.style(f'{self.dataset}', bold=1) + '..')
+        scarf.fetch_dataset(self.dataset, save_path=self.output_dir)
 
-    def read_data():
-        reader = scarf.CrH5Reader(self.outputdir + '/' + self.dataset)
-        zarr_fn= self.output_dir + '/' +  self.dataset +  '.zarr',
+    def read_data(self):
+        reader = scarf.CrH5Reader(f'{self.output_dir}/{self.dataset}/data.h5', file_type='rna')
+        zarr_fn = f'{self.output_dir}/data.zarr'
+        click.echo(f'> Creating zarr file from dataset, at ' + click.style(click.format_filename(f'{zarr_fn}'), bold=1) + '..')
         writer = scarf.CrToZarr(reader,
                 zarr_fn=zarr_fn,
                 chunk_size=(2000, 1000))
@@ -73,30 +77,35 @@ class Scarf_cli(object):
         self.ds = scarf.DataStore(zarr_fn, nthreads=self.nthreads,
                 min_features_per_cell=10)
 
-    def filter_data(attrs, highs, lows):
+    def filter_data(self, attrs = None, highs = None, lows = None):
+        click.echo('> Filtering data..')
         if attrs == None:
             self.ds.auto_filter_cells(show_qc_plots=False)
 #         ds.filter_cells(attrs=['RNA_nCounts', 'RNA_nFeatures', 'RNA_percentMito'],
 #                 highs=[15000, 4000, 15], lows=[1000, 500, 0])
 
-    def mark_hvgs():
+    def mark_hvgs(self):
+        click.echo('> Finding highly variable genes..')
         self.ds.mark_hvgs()
 
-    def construct_graph():
-        self.ds.make_graph()
+    def construct_graph(self):
+        click.echo('> Constructing graph..')
+        self.ds.make_graph(feat_key='hvgs')
         self.ds.run_umap()
-        self.ds.run_clustering()
-        self.ds.run_marker_search()
+        self.ds.run_clustering(n_clusters=12)
+        self.ds.run_marker_search(group_key='RNA_cluster')
 
-    def save_data():
-        self.ds.get_markers().to_csv(self.output_dir + '/markers.csv')
+    def save_data(self):
+        click.echo('> Saving data..')
+        self.ds.export_markers_to_csv(group_key='RNA_cluster', csv_filename=f'{self.output_dir}/markers.csv')
 
-    def run_pipeline():
-        read_data()
-        filter_data()
-        mark_hvgs()
-        construct_graph()
-        save_data()
+    def run_pipeline(self):
+        self.download_dataset()
+        self.read_data()
+        self.filter_data()
+        self.mark_hvgs()
+        self.construct_graph()
+        self.save_data()
 
 def print_datasets(ctx, param, value):
     if not value or ctx.resilient_parsing:
@@ -105,10 +114,6 @@ def print_datasets(ctx, param, value):
     ctx.exit()
 
 @click.group()
-@click.option('--dataset', default='tenx_5K_pbmc_rnaseq', type=str, show_default=True)
-@click.option('-o', '--outputdir', default='./basic-scRNA-Seq', show_default=True)
-# filenames should be handled in a specific way according to click docs
-@click.option('-j', '--nthreads', default=4, required=False)
 @click.option('--show-datasets', is_flag=True, callback=print_datasets,
         expose_value=False, is_eager=True,
         help="Show the prepared datasets available for download.")
@@ -116,8 +121,8 @@ def print_datasets(ctx, param, value):
 # can add scarf.__version__() here later
 # right now only works when pip installed
 @click.pass_context
-def cli(ctx, dataset, outputdir, nthreads):
-    ctx.obj = Scarf_cli(dataset, outputdir, nthreads)
+def cli(ctx):
+    pass
     # could just put everything into here. In the basic case, i. e. running an
     # automated analysis of a given dataset, we really don't need all fancy
     # functionality of click, we can make it easier for ourselves.
@@ -126,12 +131,17 @@ def cli(ctx, dataset, outputdir, nthreads):
     # a first step, mayhaps a PoC automated pipeline is okay.
 
 @cli.command()
-@click.pass_obj
-def run_pipeline(obj):
+@click.option('--dataset', default='tenx_5K_pbmc_rnaseq', type=str, show_default=True)
+@click.option('-o', '--outputdir', default='./basic-scRNA-Seq', show_default=True)
+# filenames should be handled in a specific way according to click docs
+@click.option('-j', '--nthreads', default=4, required=False)
+@click.pass_context
+def run_pipeline(ctx, dataset, outputdir, nthreads):
     """Run the whole pipeline, as automated as possible.
     """
-#     click.echo(obj)
-#     obj.run_pipeline()
+    ctx.obj = Scarf_cli(dataset, outputdir, nthreads)
+    click.echo(ctx.obj)
+    ctx.obj.run_pipeline()
 
 if __name__ == "__main__":
     cli()
